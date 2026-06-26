12aa82076f180fad1ad3d2e46b493e7439cae734
Nitish <sanram03@gmail.com>
2026-06-26 15:56:43 +0530

    Initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,14 +6,14 @@ export default function Navbar() {
     <nav className="bg-white shadow-sm sticky top-0 z-50">
       <div className="container flex items-center justify-between">
         <Link href="/">
-          <Image src="/sanram.logo.png" alt="Logo" width={100} height={50} />
+          <Image src="/icon.png" alt="Logo" width={100} height={50} />
         </Link>
 
         <div className="flex gap-8">
           <div className="flex gap-4">
             <Link
               href="/"
-             className="px-6 py-3 border-2 border-blue-600 text-blue-600 rounded-lg font-medium hover:bg-blue-600 hover:text-white transition duration-300"
+             className="px-6 py-3 border-2 blue-600 text-blue-600 rounded-lg font-medium hover:bg-blue-600 hover:text-white transition duration-300"
             >
               Latest News
             </Link>
