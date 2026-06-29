6e757f94933e9a265b8043578e0902ebea013d0c
Nitish <nitishdhaka162@gmail.com>
2026-06-29 17:26:44 +0530

    this is new changes

--- src/app/api/aviation-news/route.js
+++ src/app/api/aviation-news/route.js
@@ -1,17 +1,2 @@
-// app/api/aviation-news/route.ts
-
-import { NextResponse } from "next/server";
-import { getAviationNews } from "@/lib/aviationNews";
-
-export const dynamic = "force-dynamic"; // ← this is the fix
-
-export async function GET() {
-  const articles = await getAviationNews();
-
-  return NextResponse.json({
-    status: "ok",
-    totalResults: articles.length,
-    articles,
-    message: "API Working",
-  });
-}
+// src/lib/news.ts
+export { getAviationNews } from "@/lib/aviationNews";
